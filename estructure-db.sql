d82bce4a46cf0faca5d2a32fa019b0d15544ee97
Alexi_Dg <alexisdurangomez588@gmail.com>
2025-09-24 14:09:40 -0500

    feat: :fire: added tables to database

--- estructure-db.sql
+++ estructure-db.sql
@@ -1,5 +1,77 @@
--- Creación de la base de datos
-CREATE DATABASE IF NOT EXISTS CampusBike;
+-- CREACIÓN DE LA BASE DE DATOS
+CREATE DATABASE campusbike_db;
+USE campusbike_db;
 
--- Uso de la base de datos 
-use CampusBike;
+-- TABLA CLIENTES
+CREATE TABLE Clientes (
+    id_cliente INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_cliente VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    telefono VARCHAR(20)
+);
+
+-- TABLA PROVEEDORES
+CREATE TABLE Proveedores (
+    id_proveedor INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_proveedor VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    telefono VARCHAR(20)
+);
+
+-- TABLA REPUESTOS
+CREATE TABLE Repuestos (
+    id_repuesto INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    precio_unitario DECIMAL(10,2) NOT NULL
+);
+
+-- TABLA INVENTARIO
+CREATE TABLE Inventario (
+    id_inventario INT AUTO_INCREMENT PRIMARY KEY,
+    id_repuesto INT NOT NULL,
+    stock INT NOT NULL DEFAULT 0,
+    estado ENUM('disponible','agotado','en pedido') DEFAULT 'disponible',
+    FOREIGN KEY (id_repuesto) REFERENCES Repuestos(id_repuesto)
+);
+
+-- TABLA VENTAS
+CREATE TABLE Ventas (
+    id_venta INT AUTO_INCREMENT PRIMARY KEY,
+    id_cliente INT NOT NULL,
+    fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    precio_total DECIMAL(10,2) NOT NULL,
+    FOREIGN KEY (id_cliente) REFERENCES Clientes(id_cliente)
+);
+
+-- TABLA DETALLE VENTAS (RESUELVE N:M ENTRE VENTAS Y REPUESTOS)
+CREATE TABLE DetalleVenta (
+    id_detalle_venta INT AUTO_INCREMENT PRIMARY KEY,
+    id_venta INT NOT NULL,
+    id_repuesto INT NOT NULL,
+    cantidad INT NOT NULL,
+    precio_unitario DECIMAL(10,2) NOT NULL,
+    precio_total DECIMAL(10,2) NOT NULL,
+    FOREIGN KEY (id_venta) REFERENCES Ventas(id_venta),
+    FOREIGN KEY (id_repuesto) REFERENCES Repuestos(id_repuesto)
+);
+
+-- TABLA COMPRAS
+CREATE TABLE Compras (
+    id_compra INT AUTO_INCREMENT PRIMARY KEY,
+    id_proveedor INT NOT NULL,
+    fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    precio_total DECIMAL(10,2) NOT NULL,
+    FOREIGN KEY (id_proveedor) REFERENCES Proveedores(id_proveedor)
+);
+
+-- TABLA DETALLE COMPRAS (RESUELVE N:M ENTRE COMPRAS Y REPUESTOS)
+CREATE TABLE DetalleCompra (
+    id_detalle_compra INT AUTO_INCREMENT PRIMARY KEY,
+    id_compra INT NOT NULL,
+    id_repuesto INT NOT NULL,
+    cantidad INT NOT NULL,
+    precio_unitario DECIMAL(10,2) NOT NULL,
+    precio_total DECIMAL(10,2) NOT NULL,
+    FOREIGN KEY (id_compra) REFERENCES Compras(id_compra),
+    FOREIGN KEY (id_repuesto) REFERENCES Repuestos(id_repuesto)
+);
